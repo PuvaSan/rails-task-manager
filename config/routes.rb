ea86ca97f8eaf8d74be1ef72a5b96700c7a03e49
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-01 11:10:57 +0900

    fixed routing of links issues

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # read all
+  get "/tasks", to: "tasks#index", as: :tasks
+
+  # create task
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  # send to server
+  post "/tasks", to: "tasks#create", as: :create_task
+
+  # read one
+  get "/tasks/:id", to: "tasks#show", as: :task
 end
